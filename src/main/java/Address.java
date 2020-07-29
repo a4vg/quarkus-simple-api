fd42edcb1cc6642c97f9249b0d7040ac34c8554b
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-07-29 12:35:28 -0500

    Change properties private access to public

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,30 +1,13 @@
 public class Address {
-    private String country;
-    private String city;
-    private String avenue;
-    private String number;
+    public String country;
+    public String city;
+    public String avenue;
+    public String number;
 
     public Address (String _country, String _city, String _avenue, String _number) {
         this.country = _country;
         this.city = _city;
         this.avenue = _avenue;
         this.number = _number;
-    }
-
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getAvenue() {
-        return avenue;
-    }
-
-    public String getNumber() {
-        return number;
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,29 +1,13 @@
 public class Person {
-    private String name;
-    private String lastName;
-    private Integer age;
-    private Address address;
+    public String name;
+    public String lastName;
+    public Integer age;
+    public Address address;
 
     public Person(String _name, String _lastName, Integer _age, Address _address) {
         this.name = _name;
         this.lastName = _lastName;
         this.age = _age;
         this.address = _address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public Address getAddress() {
-        return address;
     }
 }
