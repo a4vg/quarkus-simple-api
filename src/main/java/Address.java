2d4dc6ff7468c59e16d90fb42c00c401dcd943bf
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-07-30 16:14:59 -0500

    Change attr access to private and create get/setters

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,8 +1,8 @@
 public class Address {
-    public String country;
-    public String city;
-    public String avenue;
-    public String number;
+    private String country;
+    private String city;
+    private String avenue;
+    private String number;
 
     public Address(){}; // needed by JSON serializer
 
@@ -11,5 +11,38 @@ public class Address {
         this.city = _city;
         this.avenue = _avenue;
         this.number = _number;
+    }
+
+    // Getters and setters
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAvenue() {
+        return avenue;
+    }
+
+    public void setAvenue(String avenue) {
+        this.avenue = avenue;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,8 +1,8 @@
 public class Person {
-    public String name;
-    public String lastName;
-    public Integer age;
-    public Address address;
+    private String name;
+    private String lastName;
+    private Integer age;
+    private Address address;
 
     public Person(){} // needed by JSON serializer
 
@@ -11,5 +11,38 @@ public class Person {
         this.lastName = _lastName;
         this.age = _age;
         this.address = _address;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
